21747c787362c9ba04cecfe0aa912397e988d466
지인혁 <87266785+wldlsgur@users.noreply.github.com>
2025-10-20 23:33:46 +0900

    feat(create-turbo): apply official-starter transform

--- apps/hook-kit/src/App.tsx
+++
@@ -1,5 +0,0 @@
-const App = () => {
-  return <h1>123</h1>;
-};
-
-export default App;
--- apps/hook-kit/src/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as useCallback } from './use-callback';
-export { default as useLazyImageLoad } from './use-lazy-load-image';
