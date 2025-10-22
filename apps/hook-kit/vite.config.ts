4fccdd972e04a4aa8f8432da96be6e679fd09d40
지인혁 <87266785+wldlsgur@users.noreply.github.com>
2025-10-22 23:26:24 +0900

    feat: 빌드 환경 구축

--- apps/hook-kit/vite.config.ts
+++ apps/hook-kit/vite.config.ts
@@ -1,8 +1,47 @@
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  root: __dirname,
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    dts({
+      rollupTypes: true,
+      root: resolve(__dirname),
+      tsconfigPath: resolve(__dirname, './tsconfig.app.json'),
+      entryRoot: resolve(__dirname, 'src'),
+      include: [resolve(__dirname, 'src/**/*.{ts,tsx}')],
+      insertTypesEntry: true,
+      exclude: [
+        '**/*.stories.tsx',
+        '**/*.test.ts',
+        '**/*.spec.ts',
+        '**/__tests__/**',
+        'App.tsx',
+        'main.tsx',
+      ],
+    }),
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'HookKit',
+      fileName: 'index',
+      formats: ['es', 'cjs', 'umd'],
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+    emptyOutDir: true,
+  },
 });
